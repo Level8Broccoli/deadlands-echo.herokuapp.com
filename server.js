e8622b2c6c854776f2db8d95cbce0b6d4deff34f
Oliver Bucher <level8broccoli@gmail.com>
2020-04-30 16:26:04 +0200

    add userlist

--- server.js
+++ server.js
@@ -2,29 +2,45 @@ var http = require('http');
 var sockjs = require('sockjs');
 var node_static = require('node-static');
 
+var user_list = {};
+
+function broadcast(message) {
+  for (var user in user_list) {
+    user_list[user].write(JSON.stringify(message));
+  }
+}
+
 // 1. Echo sockjs server
-var sockjs_opts = {sockjs_url: "http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js"};
+var sockjs_opts = {
+  sockjs_url: "http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js"
+};
 
 var sockjs_echo = sockjs.createServer(sockjs_opts);
-sockjs_echo.on('connection', function(conn) {
-    conn.on('data', function(message) {
-        conn.write(message);
-    });
+sockjs_echo.on('connection', function (conn) {
+  user_list[conn.id] = conn;
+  conn.on('data', function (message) {
+    broadcast(JSON.parse(message));
+  });
+  conn.on("close", function () {
+    delete user_list[conn.id];
+  })
 });
 
 // 2. Static files server
-var static_directory = new node_static.Server(__dirname);
+// var static_directory = new node_static.Server(__dirname);
 
 // 3. Usual http stuff
 var server = http.createServer();
-server.addListener('request', function(req, res) {
-    static_directory.serve(req, res);
+server.addListener('request', function (req, res) {
+  static_directory.serve(req, res);
 });
-server.addListener('upgrade', function(req,res){
-    res.end();
+server.addListener('upgrade', function (req, res) {
+  res.end();
 });
 
-sockjs_echo.installHandlers(server, {prefix:'/echo'});
+sockjs_echo.installHandlers(server, {
+  prefix: '/echo'
+});
 const port = process.env.PORT || 3000;
-console.log(` [*] Listening on 0.0.0.0:${port}` );
+console.log(` [*] Listening on 0.0.0.0:${port}`);
 server.listen(port, '0.0.0.0');
